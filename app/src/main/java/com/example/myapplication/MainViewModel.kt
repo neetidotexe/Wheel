fb22483416c6bd2af5c4bba11aeeb975c6f41ada
Neeti <neeti@playo.co>
2020-07-16 10:28:04 +0530

    Add Validation function in ViewModel to check if user input is appropriate

--- app/src/main/java/com/example/myapplication/MainViewModel.kt
+++ app/src/main/java/com/example/myapplication/MainViewModel.kt
@@ -3,6 +3,34 @@ package com.example.myapplication
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class MainViewModel: ViewModel() {
+class MainViewModel : ViewModel() {
     var progressValue = MutableLiveData<Double>()
+    var errorType = MutableLiveData<ErrorType>()
+
+    fun onClickSubmit(progressText: String?) {
+        if (progressText.isNullOrEmpty()) {
+            errorType.value = ErrorType.EMPTY_INPUT
+            return
+        }
+
+        val progress = try {
+            progressText.toDouble()
+        } catch (ex: NumberFormatException) {
+            ex.printStackTrace()
+            -1.0
+        }
+
+        if (progress in 0.0..100.0) {
+            errorType.value = ErrorType.NONE
+            progressValue.value = progressText.toDouble()
+        } else {
+            errorType.value = ErrorType.INVALID_INPUT
+        }
+    }
+}
+
+enum class ErrorType {
+    INVALID_INPUT,
+    EMPTY_INPUT,
+    NONE
 }
