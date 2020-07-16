3eccc35e1cc1c1318bac72025f455710738127c0
Neeti <neeti@playo.co>
2020-07-16 10:55:54 +0530

    Create ViewModel object and observe LiveData in MainActivity

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var enterProgress : EditText
     private lateinit var animateButton : Button
+    private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,9 +20,26 @@ class MainActivity : AppCompatActivity() {
 
         enterProgress = findViewById(R.id.text_progress)
         animateButton = findViewById(R.id.button_animate)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
         animateButton.setOnClickListener {
-
+            val progressString = enterProgress.text.toString()
+            viewModel.onClickSubmit(progressString)
         }
+
+        viewModel.errorType.observe(this, Observer {
+            when(it){
+                ErrorType.INVALID_INPUT -> showInvalidInputError()
+                ErrorType.EMPTY_INPUT -> showEmptyInputError()
+            }
+        })
+    }
+
+    private fun showInvalidInputError(){
+        Toast.makeText(this, "Progress can not be more than 100", Toast.LENGTH_LONG).show()
+    }
+
+    private fun showEmptyInputError(){
+        Toast.makeText(this, "Please enter a progress value", Toast.LENGTH_LONG).show()
     }
 }
