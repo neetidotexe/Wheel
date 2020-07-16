77049fb6734cb44698b5cf58c811d191cf1b44b8
Neeti <neeti@playo.co>
2020-07-16 09:11:27 +0530

    Add EditText and Button in MainActivity.kt

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,11 +2,23 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var enterProgress : EditText
+    private lateinit var animateButton : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        enterProgress = findViewById(R.id.text_progress)
+        animateButton = findViewById(R.id.button_animate)
+
+        animateButton.setOnClickListener {
+
+        }
     }
 }
