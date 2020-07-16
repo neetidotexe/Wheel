48eb4ef9a5f9213f5f32d7335e72140c701db15b
Neeti <neeti@playo.co>
2020-07-16 11:20:06 +0530

    Create Custom View Class namely CircularProgressBar

---
+++ app/src/main/java/com/example/myapplication/CirclularProgressBar.kt
@@ -0,0 +1,8 @@
+package com.example.myapplication
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class CirclularProgressBar(context: Context, attrs: AttributeSet?) : View(context , attrs) {
+}
